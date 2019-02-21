56516493fa0538d7ad57c9f09b79293195de4e26
Eduardo Cuducos <cuducos@gmail.com>
2019-02-20 21:33:39 -0300

    Fix pytext missing fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,19 @@
-from my_internet_speed.models import Result
+import pytest
+
+from my_internet_speed.backends.speed_test_net import SpeedTest
 
 
-def test_run(speed_test_net):
-    results = speed_test_net.run()
-    expected = Result(
-        download=22_705_960.903_082_576,
-        upload=6_743_478.793_460_394,
-        ping=21.134,
-        timestamp="1970-01-01T00:00:00.000000Z",
-        bytes_sent=11_255_808,
-        bytes_received=28_753_668,
-        server={
+@pytest.fixture
+def speed_test_net(mocker):
+    share = f"http://www.speedtest.net/result/42.png"
+    results = {
+        "download": 22_705_960.903_082_576,
+        "upload": 6_743_478.793_460_394,
+        "ping": 21.134,
+        "timestamp": "1970-01-01T00:00:00.000000Z",
+        "bytes_sent": 11_255_808,
+        "bytes_received": 28_753_668,
+        "server": {
             "url": "http://example.com/speedtest/upload.php",
             "lat": "-34.0098",
             "lon": "-56.6573",
@@ -24,7 +27,7 @@ def test_run(speed_test_net):
             "d": 3.141_592_653_589_793_2,
             "latency": 31.415,
         },
-        client={
+        "client": {
             "ip": "192.168.0.1",
             "lat": "-36.4679",
             "lon": "-39.3789",
@@ -36,11 +39,8 @@ def test_run(speed_test_net):
             "loggedin": "0",
             "country": "XX",
         },
-        url="http://www.speedtest.net/result/42",
-    )
-
-    speed_test_net.client.get_best_server.assert_called_once_with()
-    speed_test_net.client.download.assert_called_once_with()
-    speed_test_net.client.upload.assert_called_once_with()
-    for field in Result._meta.fields.keys():
-        assert getattr(expected, field) == getattr(results, field)
+    }
+    mock = mocker.patch("my_internet_speed.backends.speed_test_net.Speedtest")
+    mock.return_value.results.dict.return_value = results
+    mock.return_value.results.share.return_value = share
+    return SpeedTest()
